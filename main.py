a48d9b85b1d8bd0d5766324e994dccc5734c2982
Willow-Zero <zmckinnis4@gmail.com>
2026-05-19 20:20:10 -0400

    added gutenberg handling, split out the handling for txts because its easier to do little finctions. also spliut it to go through biblioFile, as that simplifies the coder.

--- main.py
+++ main.py
@@ -4,20 +4,66 @@ import random
 USEDIR = os.getcwd() + '/bibliomancy'
 USEFILE=False
 
-def biblioDir(directory):
-    files = os.listdir(directory)
-    targetBib = directory + '/' + random.choice(files)
-    with open(targetBib) as f:
-        splitup = (f.read().split("\n\n")) #double newline because proj gutenberg books use single newline for typographical purposes and double as a paragraph division. ideally id have better control over the input texts - maybe test for other evidence of PG formatting and exclude the license text and split by double newline with newline as default? idk
+
+
+def txtHandler(file):
+    splitter = "\n"
+    gutFlag = False
+    with open(file) as f:
+        if "PROJECT GUTENBERG EBOOK" in f.read():
+            splitter = "\n\n"
+            gutFlag = True
+        splitup = (f.read().split(splitter)) #double newline because proj gutenberg books use single newline for typographical purposes and double as a paragraph division. ideally id have better control over the input texts - maybe test for other evidence of PG formatting and exclude the license text and split by double newline with newline as default? idk
         poppable = []
+        after = len(splitup)+50
         for sector in range(len(splitup)):
             if splitup[sector].strip == "":
+                poppable.append(sector)
+            if gutFlag and "END OF THE PROJECT GUTENBERG EBOOK" in splitup[sector]:
+                after = sector
+                poppable.append(sector)             # after end, its just license text, and i dont want to be bibliomancing license text. 
+            if gutFlag and "The Project Gutenberg eBook of" in splitup[sector]:
+                poppable.append(sector)
+            if gutFlag and "This eBook is for the use of anyone anywhere in the United States" in splitup[sector]:
+                poppable.append(sector)
+            if gutFlag and "Title:" in splitup[sector]:
+                poppable.append(sector)
+            if gutFlag and "Release date:" in splitup[sector]:
+                poppable.append(sector)                                         # these all strip elements of the project gutenberg pre-text from the randomness. 
+            if gutFlag and "Language:" in splitup[sector]:
+                poppable.append(sector)
+            if gutFlag and "Other information and formats:" in splitup[sector]:
+                poppable.append(sector)
+            if gutFlag and "*** START OF THE PROJECT GUTENBERG EBOOK" in splitup[sector]:
+                poppable.append(sector)
+            if sector>after:
                 poppable.append(sector)
         i=len(poppable)-1
         while (i>=0):
             splitup.pop(i)
             i-1
         return random.choice(splitup)
+
+
+
+
+
+
+
+def biblioFile(file):
+    if file.split(".")[-1].toLower in ["txt","md"]: 
+        txtHandler(file)
+
+
+
+
+
+
+
+def biblioDir(directory):
+    files = os.listdir(directory)
+    targetBib = directory + '/' + random.choice(files)
+    biblioFile(targetBib)
                 
     
 
